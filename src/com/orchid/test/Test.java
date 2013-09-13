fc765dcf9db390f026b15dd5208af39d856d7a87
yang-qun <tokoto@qq.com>
2013-09-13 20:59:13 +0800

    base

---
+++ src/com/orchid/test/Test.java
@@ -0,0 +1,23 @@
+package com.orchid.test;
+
+public class Test {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
